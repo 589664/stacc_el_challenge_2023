cb985386211ed1c8b00f0f778a90c4e98868e768
Maksim <589664@stud.hvl.no>
2023-02-20 00:37:29 +0100

    added tailwind css

---
+++ src/components/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <main className="mx-auto w-full max-w-4xl px-3 sm:px-10">{children}</main>
+      <div className="flex-grow" />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,24 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
+// import Image from "next/image";
+// import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-
-// export default function Home() {
-//   return (
-//     <>
-//       <Head>
-//         <title>Sparstrøm</title>
-//       </Head>
-
-//       <Header />
-
-//       <div className={styles.content}>
-//         <h1 className={styles.title}>Sparstrøm</h1>
-//         <p className={styles.description}>
-//           Denne nettsiden skal vise deg hvordan du kan spare penger på strøm,
-//           ved å samenlikne up to date priser fra flere leverandører
-//         </p>
-//       </div>
-
-//       {/* <Footer /> */}
-//     </>
-//   );
-// }
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>Sparstrøm</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Header />
-      <main style={{ minHeight: "calc(85vh)", marginTop: '100px' }}>
-        <h1 className={styles.title}>Sparstrøm</h1>
-        <p className={styles.description}>
-          Denne nettsiden skal vise deg hvordan du kan spare penger på strøm,
-          ved å samenlikne up to date priser fra flere leverandører
-        </p>
-      </main>
-      <Footer />
-    </div>
+      <div className="flex flex-col gap-2 text-xl font-extralight">
+        <main style={{ minHeight: "calc(85vh)", marginTop: "100px" }}>
+          <h1 className={styles.title}>Sparstrøm</h1>
+          <p className={styles.description}>
+            Denne nettsiden skal vise deg hvordan du kan spare penger på strøm,
+            ved å samenlikne up to date priser fra flere leverandører
+          </p>
+        </main>
+      </div>
+    </>
   );
 };
 
